1b1feadc6e127daf6a61b62d337bfe813e8844f6
sofi131 <sofiasanmartin131@gmail.com>
2024-05-24 17:02:21 +0200

    user.js y una demo de prueba

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+//const Schema = mongoose.Schema;
+
+// Definir el esquema del usuario
+const userSchema = new Schema({
+    name:
+    {
+        type: String,
+        required: true,
+        // unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:
+    {
+        type: String,
+        required: true
+    }
+
+});
+
+// Crear el modelo del usuario
+module.exports = mongoose.model('User', userSchema);

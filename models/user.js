503f193ad2b91dc5e07e7ca3de302d851c2f8b3f
sofi131 <sofiasanmartin131@gmail.com>
2024-05-24 18:08:12 +0200

    user.js y bcrypt

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:false,
-        trim:true
+    name: {
+        type: String,
+        required: false,
+        trim: true
     },
     email: {
         type: String,
@@ -16,5 +17,17 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password') || this.isNew) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
